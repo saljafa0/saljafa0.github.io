decad115187cd31ca7ee51b54ae26ddf6f43e36a
saljafa0 <saljafa0@email.cpcc.edu>
2026-02-09 23:18:19 -0500

    changes

--- web115/scripts/fizzbuzz0.js
+++ web115/scripts/fizzbuzz0.js
@@ -19,13 +19,15 @@ function makeGreeting() {
     // Replace the content of the greeting element with the new greeting message
     document.getElementById("greeting").textContent = newGreeting;
 
-
+    //call the loop function to run the loop challenge
+    runNumLoop(firstName);
+}
     //=======LOOP CHALLENGE STARTS HERE========
 
-
+function runNumLoop(firstName) {
     // Prompt user to enter a number
     let limit = prompt(`Please enter a number count, ${firstName}: `);
-    //Validate the input
+    //Validate the input case for empty string,spaces, or non-numeric value 
     if (limit === "" || isNaN(limit)) {
         alert("Please enter a valid number.");
         return;
@@ -34,11 +36,11 @@ function makeGreeting() {
     // Get the loop element
     const loopElement = document.getElementById("loop");
 
-    // Create list items for each number from 1 to the entered number using loop and determine if it's even or odd
+    // Create list items for each number from 1 to the entered number using loop and determine if it's even or odd using modulus operator
     let listItems = "";
     for (let i = 1; i <= limit; i++) {
-        let evenOdd = (i % 2 === 0) ? "even" : "odd";
-        listItems += `<li>${i} Glazed Glory - The number is ${evenOdd}</li>`;
+        const evenOdd = ["even", "odd"]
+        listItems += `<li>${i} Glazed Wood - The number is ${evenOdd[i % 2]}</li>`;
     }
     loopElement.innerHTML = listItems;
 
