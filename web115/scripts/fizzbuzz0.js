5fd4ae3fb89b4595cd261024bbbc2461e5a3192b
saljafa0 <saljafa0@email.cpcc.edu>
2026-02-10 00:23:43 -0500

    Update fizzbuzz0.js

--- web115/scripts/fizzbuzz0.js
+++ web115/scripts/fizzbuzz0.js
@@ -32,21 +32,16 @@ function makeGreeting() {
     const lastName = document.getElementById("last-name").value.trim();
 
     //omit middle name if it's empty
-    fixedMiddleName = "";
+    let fixedMiddleName = "";
     if (middleName === "") {
-        fixedMiddleName = "";
-    } else {
-        fixedMiddleName = middleName + ". " ;//add a period and space after the middle initial if it's not empty
-    }
+        fixedMiddleName = middleName + ". ";
+    } 
 
     let fullName = `${firstName} ${fixedMiddleName}${lastName}`.trim(); //combine the names into a full name and trim any extra spaces
     //shows something if first or last name is empty, otherwise it will show the greeting message with the full name
     if (firstName === "" && lastName === "") {
         fullName = "Valued Guest";
-    } else {
-        let fixedMiddleName = middleName ? middleName + '. ' : ''; //use a ternary operator to check if middle name is empty and add a period and space if it's not
-        fullName = `${firstName} ${fixedMiddleName}${lastName}`.trim();
-    }
+    } 
 
     // Create the greeting message
     const newGreeting = `Welcome to Wild Wood Furniture, ${fullName}!`;
